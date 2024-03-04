4d562fb128d503cde4a1b1e00200c579b85b86b5
Rumsie-Grimshaw <cradleofrumsie@gmail.com>
2024-03-04 22:59:46 +0800

    Added 3 more tests for a total of 5 tests, including win condtions and stalemate conditions.

--- tests/test_tic_tac_toe.py
+++ tests/test_tic_tac_toe.py
@@ -10,7 +10,6 @@ class TestTicTacToe(unittest.TestCase):
 
     # Check value entered by player is appended to correct board space
     def test_input_is_correct(self):
-
         # Arrange
         current_player = 'X'
         value = 7
@@ -24,7 +23,6 @@ class TestTicTacToe(unittest.TestCase):
 
     # Check that values entered on a space that is already filled will not be overwritten. Test Index = 5
     def test_space_already_taken(self):
-
         # Arrange
         first_player = '0'
         second_player = 'X'
@@ -38,3 +36,37 @@ class TestTicTacToe(unittest.TestCase):
 
         # Assert
         self.assertEqual(result, first_player)
+
+    # Check that mock board state is a successful win condition
+    def test_is_winning_condition(self):
+        # Arrange
+        self.game_board.board = ['X', 'X', 'X', ' ', 'O', ' ', 'O', ' ', 'O']
+
+        # Act
+        result = self.game_board.is_win_condition_met()
+
+        # Assert
+        self.assertTrue(result)
+
+    # Check that mock board state does not award a false diagonal win
+    def test_is_diagonal_win_condition(self):
+        # Arrange
+        self.game_board.board = ['X', ' ', 'O', 'X', 'X', ' ', 'O', ' ', 'O']
+
+        # Act
+        result = self.game_board.is_win_condition_met()
+        self.game_board.display_board()
+
+        # Assert
+        self.assertFalse(result)
+
+    # Check that mock diagonal state is a successful win condition
+    def test_game_results_in_a_draw(self):
+        # Arrange
+        self.game_board.board = ['X', 'O', 'X', 'O', 'O', 'X', 'O', 'X', 'O']
+
+        # Act
+        result = self.game_board.is_win_condition_met()
+
+        # Assert
+        self.assertTrue(result)
