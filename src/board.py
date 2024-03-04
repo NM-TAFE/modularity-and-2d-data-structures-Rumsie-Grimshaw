4881f7d73f94459b6f49a8b779d8fa148f4f3ad1
Rumsie-Grimshaw <cradleofrumsie@gmail.com>
2024-03-04 22:58:06 +0800

    Added bool return values to is_win_condition to assist with testing and to provide more concrete functionality.

--- src/board.py
+++ src/board.py
@@ -15,21 +15,25 @@ class GameBoard:
     def update_board(self, current_player, value):
         if self.board[int(value)] == self.empty:
             self.board[int(value)] = current_player
-        return game_board
+        return self
 
     def is_win_condition_met(self):
         win_conditions = [(0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6)]
+
         # Cross-reference index with win_conditions to determine if game is won.
         for index in win_conditions:
             if self.board[index[0]] == self.board[index[1]] == self.board[index[2]] != self.empty:
                 self.display_board()
-                print("Player", game_board.board[index[0]], "wins!")
-                exit(0)
-        # Assess if game state is a draw by evaluating empty spaces.
+                print("Player", self.board[index[0]], "wins!")
+                return True
+
+        # Assess if the game state is a draw by evaluating empty spaces.
         if self.empty not in self.board:
             self.display_board()
             print("It's a tie!")
-            exit(0)
+            return True
+
+        return False
 
 
 game_board = GameBoard()
