977fb116fa839a1ba8dddd6da00461b3fe76a594
Rumsie-Grimshaw <cradleofrumsie@gmail.com>
2024-03-04 16:33:45 +0800

    Moved is_win_condition under other methods for readability and encased program loop in a start function.

--- src/game.py
+++ src/game.py
@@ -1,38 +1,39 @@
-import board
+from board import game_board
 
-# Players one and two
-player_one = "X"
-player_two = "O"
 
-# Check if winning conditions met.
-def is_winning_condition():
-    board.is_win_condition_met()
-
-# Check remaining empty spaces on board and determine if odd or even number remains. Odd = player_one Even == player_two
+# Check remaining spaces on board and assign player based on odd or even number of remaining empty spaces.
+# Player One = Even and Player Two = Odd
 def get_player():
-    if board.board.count(board.empty) % 2 != 0:
-        current_player = player_one
+    if game_board.board.count(game_board.empty) % 2 != 0:
+        current_player = "X"
     else:
-        current_player = player_two
+        current_player = "O"
     return current_player
 
-def player_input():
+def player_input(player):
     # prints a message denoting the current players turn and asks for user input
     player_move = input("Next move for player " + player + " (0-8): ")
 
     # Check is choice is an int, between 0 - 8 and that the space is empty. If so, fill space, else; request new number.
-    while board.board[int(player_move)] == board.empty:
+    while game_board.board[int(player_move)] == game_board.empty:
         if player_move.isdigit():
             if 0 <= int(player_move) <= 8:
-                board.board[int(player_move)] = player
+                game_board.board[int(player_move)] = player
         else:
             # display error message and repeat loop
-            print("Invalid move, try again.")
+            print("Invalid input, please enter a valid number between (0-8):")
+
+# Check if winning conditions met.
+def is_winning_condition():
+    game_board.is_win_condition_met()
+
+# Begin the game
+def start_game():
+    while True:
+        game_board.display_board()
+        player = get_player()
+        player_input(player)
+        is_winning_condition()
 
 
-# Game loop
-while True:
-    board.display_board()
-    player = get_player()
-    player_input()
-    is_winning_condition()
+start_game()
