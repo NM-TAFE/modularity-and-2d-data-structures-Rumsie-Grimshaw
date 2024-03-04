24a7511e66b27ae0c6b8e37e96dfea93e4581b6d
Rumsie-Grimshaw <cradleofrumsie@gmail.com>
2024-03-04 22:56:55 +0800

    Added bool variable to start_game and is_winning_condition to assist with testing and dictate if game is completed or not.

--- src/game.py
+++ src/game.py
@@ -4,21 +4,21 @@ from board import game_board
 # Player One = Even and Player Two = Odd
 def get_player():
     if game_board.board.count(game_board.empty) % 2 != 0:
-        current_player = "X"
+        return "X"
     else:
-        current_player = "O"
-    return current_player
+        return "0"
 
 def get_player_input(current_player):
     # prints a message denoting the current players turn and asks for user input
     while True:
         print("Next move for player " + current_player + " (0-8): ")
         player_input = input()
+
         if player_input.isdigit():
-            if 0 <= int(player_input) <= 8 and game_board.board[int(player_input)] == game_board.empty:
+            index = int(player_input)
+
+            if 0 <= int(player_input) <= 8 and game_board.board[index] == game_board.empty:
                 return player_input
-            elif 0 <= int(player_input) <= 8 and game_board.board[int(player_input)] != game_board.empty:
-                print("Space already taken!")
             else:
                 print("Invalid input!")
         else:
@@ -28,16 +28,19 @@ def get_player_input(current_player):
 
 # Check if winning conditions met.
 def is_winning_condition():
-    game_board.is_win_condition_met()
+    win_condition = False
+    win_condition = game_board.is_win_condition_met()
+    return win_condition
 
 # Begin the game
 def start_game():
-    while True:
+    game_completed = False
+    while not game_completed:
         game_board.display_board()
         current_player = get_player()
         value = get_player_input(current_player)
         game_board.update_board(current_player, value)
-        is_winning_condition()
+        game_completed = is_winning_condition()
 
 
 if __name__ == '__main__':
