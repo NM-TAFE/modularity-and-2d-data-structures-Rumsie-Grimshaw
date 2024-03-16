a6dbb3668fe43c2dfa38944c6fb0150fe716bcb2
Rumsie-Grimshaw <cradleofrumsie@gmail.com>
2024-03-16 22:38:01 +0800

    Added comments to code.

--- src/game_manager.py
+++ src/game_manager.py
@@ -2,6 +2,7 @@ class GameManager:
     def __init__(self):
         ...
 
+    # Prints the board to screen
     def display_board_state(self, board_state):
         column_count = 0
         for row in board_state:
@@ -11,7 +12,7 @@ class GameManager:
                 print("-" * len(string))
                 column_count += 1
 
-
+    # Returns a value that represents the amount of remaining empty board spaces
     def return_count_empty_spaces(self, board_state):
         count = 0
         for row in board_state:
@@ -20,38 +21,45 @@ class GameManager:
                     count += 1
         return count
 
+    # Sets the player based; determined by analysing if remaining spaces are odd or even.
     def get_player(self, empty_spaces):
         if empty_spaces % 2 != 0:
             return "X"
         else:
             return "0"
 
+    # Ask player for input and validates input
     def get_player_input(self, total_spaces, current_player):
         acceptable_range = False
         while not acceptable_range:
             print(f"Next move for player {current_player} (0-{total_spaces}): ")
             player_input = input()
 
+            # Checks that players input is both an int, and less than the equal or less than total board spaces;
+            # else error.
             if player_input.isdigit() and 0 <= int(player_input) and int(player_input) <= total_spaces:
                 return player_input
             else:
                 print("Invalid input!")
 
+    # Checks if space is empty and returns boolean value.
     def space_availability(self, board_state, selected_space):
-        column, row = selected_space
-        if board_state[column][row] == " ":
+        row, column = selected_space
+        if board_state[row][column] == " ":
             return True
         else:
             return False
 
+    # Stores player char in 2D data structure if array index is empty; else error.
     def alter_board_space(self, board_state, current_player, is_empty_space, selected_space):
-        column, row = selected_space
+        row, column = selected_space
         if not is_empty_space:
             print("Invalid input! Space is already taken!")
         else:
-            board_state[column][row] = current_player
+            board_state[row][column] = current_player
 
 
+    # Determine if win conditions are met.
     def win_condition_met(self, board_state, current_player):
         # TODO: Make modular!
         win_conditions = [
--- src/main.py
+++ src/main.py
@@ -7,25 +7,25 @@ game_board = GameBoard()
 test = Test()
 
 
-# Main()
+# The function that begins the game and is called from if __name__ = '__main__'
 def main():
     start_game()
 
 
+# Game loop that runs until completion
 def start_game():
+    # Initial Game Setup
     game_board.generate_board()
     board_state = game_board.return_board_state()
-    # test.testAddThreeNumbersToFirstThreeRows(game_board)
     manager.display_board_state(board_state)
     total_spaces = game_board.return_total_board_spaces()
     game_completed = False
 
+    # Game loop starts here
     while not game_completed:
         empty_spaces = manager.return_count_empty_spaces(board_state)
         current_player = manager.get_player(empty_spaces)
-        # test.testDisplayCurrentPlayer(current_player)
         player_move = manager.get_player_input(total_spaces, current_player)
-        test.testReturnPlayerMove(f"Player move is: [{player_move}]")
         selected_space = game_board.return_board_index(player_move)
         is_empty_empty = manager.space_availability(board_state, selected_space)
         manager.alter_board_space(board_state, current_player, is_empty_empty, selected_space)
@@ -33,6 +33,7 @@ def start_game():
         manager.display_board_state(board_state)
         win_condition = manager.win_condition_met(board_state, current_player)
 
+        # Assessment of winning condition and pathways based on result
         if win_condition:
             game_completed = True
         if not win_condition:
