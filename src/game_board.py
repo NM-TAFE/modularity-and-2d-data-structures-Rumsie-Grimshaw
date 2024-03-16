db2b7d25bf1c6782517dfad582d2caf287f816c9
Rumsie-Grimshaw <cradleofrumsie@gmail.com>
2024-03-16 22:37:23 +0800

    Swapped incorrect naming of row/column to properly represent variables. Added comments.

--- src/game_board.py
+++ src/game_board.py
@@ -9,37 +9,41 @@ class GameBoard:
     def __str__(self):
         return f"This is a game board!"
 
+    # Generates the object 2D data structure
     def generate_board(self):
         for row in range(self.column):
             row = [self.empty] * self.row
             self.board.append(row)
         return self.board
 
+    # Return the current state of the objects 2D data structure indexes
     def return_board_state(self):
         return self.board
 
+    # Return a total count of the 2D data structure indexes
     def return_total_board_spaces(self):
         return self.total_space
 
+    # Iterate each index of row; increasing row # and resetting column count on each iteration until index found.
+    # Return index when found.
     def return_board_index(self, player_input):
         index = 0
-        count_x = 0
-        count_y = 0
+        column = 0
+        row = 0
 
-        for row in self.board:
-            for spaces in range(0, len(row)):
-                for space in range(0, len(row)):
-                    # test.testDisplayIndex(index, count_x, count_y)
-                    if count_x == len(row):
-                        count_x = 0
-                        count_y += 1
+        for rows in self.board:
+            for spaces in range(0, len(rows)):
+                for space in range(0, len(rows)):
+                    if column == len(rows):
+                        column = 0
+                        row += 1
 
                     elif index == int(player_input):
-                        board_space = count_y, count_x
+                        board_space = row, column
                         return board_space
 
                     else:
-                        count_x += 1
+                        column += 1
                         index += 1
 
 
